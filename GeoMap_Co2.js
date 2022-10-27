f86d68a50ef6808dfc74d4dad2ea9d9a8cb138f7
SACcustom <89899662+SACcustom@users.noreply.github.com>
2022-10-27 16:47:38 +0530

    Update GeoMap_Co2.js

--- GeoMap_Co2.js
+++ GeoMap_Co2.js
@@ -281,10 +281,10 @@
         dataSource.features.push(turf.point(dataRow.coordinates, {
           amount: (() => {
             switch (kpiName) {
-              case 'CO2': return parseInt(dataRow.debtAmount);
+              case 'Revenue': return parseInt(dataRow.debtAmount);
               case 'Exception': return parseInt(dataRow.exceptionAmount);
               case 'Service': return parseInt(dataRow.serviceAmount);
-              case 'Revenue': return parseInt(dataRow.revenueAmount);
+              case 'CO2': return parseInt(dataRow.revenueAmount);
             }
           })(),
           debtAmount: dataRow.debtAmount,
