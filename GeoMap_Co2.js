bb90599e3f81b45f7b0263b2513a34781dedfe31
SACcustom <89899662+SACcustom@users.noreply.github.com>
2022-11-02 11:54:54 +0530

    Update GeoMap_Co2.js

--- GeoMap_Co2.js
+++ GeoMap_Co2.js
@@ -316,7 +316,7 @@
         'paint': {
           // 'fill-extrusion-color': '#00f',
           'fill-extrusion-color': ['get', 'color'],
-          'fill-extrusion-height': ['get', 'amount'],
+          'fill-extrusion-height': ['get', 'amount * 15'],
           'fill-extrusion-base': 0,
           'fill-extrusion-opacity': 0.9
         }
